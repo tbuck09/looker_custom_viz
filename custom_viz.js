0b2994e49c6460b2aa47bb7145639d0f7143f6cf
Overlord | <tylerbuck09@gmail.com>
2022-04-07 13:22:13 -0600

    Custom Viz file

---
+++ custom_viz.js
@@ -0,0 +1,8 @@
+looker.plugins.visualizations.add({
+    create: function(element, config) {
+  
+    },
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+  
+    }
+})
